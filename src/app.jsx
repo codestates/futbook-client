faadd2852fb3689263efad2ff1ce752a677b9351
민제현 <alswpgus0@gmail.com>
2021-04-28 20:34:18 +0900

    feature_loginCompo_#1:Added loginComponent

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,19 @@
 // import React from 'react';
 import styles from "./app.module.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
+import Login from "./pages/login/login";
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path=""></Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
