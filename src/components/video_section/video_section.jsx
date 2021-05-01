404dcf1f1969aaeb1ebaba0cdcb9f38ca117527a
Jekyung-Min <cwm7485@naver.com>
2021-05-01 16:47:41 +0900

    feature_makeVideoSection_#10: made videoSection and modified signin,signup btn

---
+++ src/components/video_section/video_section.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "../button/button";
+import styles from "./video_section.module.css";
+function VideoSection() {
+  return (
+    <div className={styles.container}>
+      <video src="/videos/free_football_video.mp4" autoPlay loop muted />
+      <h1>풋살장 예약은 풋북.com</h1>
+      <p>더이상 기다리지 마세요!</p>
+      <div className={styles.btns}>
+        <Button className={styles.btn} buttonStyle="two" buttonSize="large">
+          예약하기
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default VideoSection;
