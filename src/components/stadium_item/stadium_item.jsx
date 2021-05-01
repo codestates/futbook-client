bba6332e97c55ae4d6ae7223e95ad5cac4b8ba2f
Jekyung-Min <cwm7485@naver.com>
2021-05-02 05:20:24 +0900

    feature_makeList_#17: divided into inside, outside

--- src/components/stadium_item/stadium_item.jsx
+++ src/components/stadium_item/stadium_item.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./stadium_item.module.css";
-function StadiumItem(props) {
+const StadiumItem = ({ path, label, img, title }) => {
   return (
     <>
       <li className={styles.item}>
-        <Link className={styles.link} to={props.path}>
-          <figure className={styles.wrap} data-category={props.label}>
-            <img className={styles.pic} alt="stadium" src={props.src} />
-          </figure>
+        <Link className={styles.link} to={path}>
+          <div
+            className={
+              label === "실외"
+                ? `${styles.wrap}`
+                : `${styles.wrap} ${styles.inside}`
+            }
+            data-category={label}
+          >
+            <img className={styles.pic} alt="stadium" src={img} />
+          </div>
+
           <div className={styles.info}>
-            <h5 className={styles.info_text}>{props.text}</h5>
+            <h5 className={styles.info_text}>{title}</h5>
           </div>
         </Link>
       </li>
     </>
   );
-}
+};
 
 export default StadiumItem;
