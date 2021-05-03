3b428ddb98b40c96feb6edce24e630daf2cb7fd9
Jekyung-Min <cwm7485@naver.com>
2021-05-03 14:24:38 +0900

    #19: add modal.jsx

---
+++ src/components/modal/modal.jsx
@@ -0,0 +1,56 @@
+import React, { useRef, useEffect, useCallback } from "react";
+import { useSpring, animated } from "react-spring";
+import styles from "./modal.module.css";
+
+export const Modal = ({ showModal, setShowModal }) => {
+  const backRef = useRef();
+
+  const animation = useSpring({
+    config: {
+      duration: 300,
+    },
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(4%)` : `translateY(-100%)`,
+  });
+
+  const closeModal = (e) => {
+    if (backRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    (e) => {
+      if (showModal && e.key === "Escape") {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+    return () => document.removeEventListener("keydown", keyPress);
+  }, [keyPress]);
+
+  return (
+    <>
+      {showModal ? (
+        <div className={styles.back} onClick={closeModal} ref={backRef}>
+          <animated.div style={animation}>
+            <div className={styles.wrap}>
+              <div>
+                <h1>Are you ready?</h1>
+                <p>Get exclusive access to our next launch.</p>
+                <button>Join Now</button>
+                <div onClick={() => setShowModal((prev) => !prev)}>
+                  <i className="fas fa-times"></i>
+                </div>
+              </div>
+            </div>
+          </animated.div>
+        </div>
+      ) : null}
+    </>
+  );
+};
