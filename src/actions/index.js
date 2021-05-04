4ef6fd75b94e1201ca16a065fb08d3f8d17c1984
민제현 <alswpgus0@gmail.com>
2021-05-04 22:51:20 +0900

    feature_dispatchMiddleware_#32:Added mddilewares

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // action types
 // sign part
 export const SIGN_IN = "SIGN_IN";
@@ -9,9 +11,31 @@ export const CHECK_BOOK = "CHECK_BOOK";
 // futsal part
 export const GET_ALLFUTSAL_INFO = "GET_ALLFUTSAL_INFO";
 
+//middleware
+export const fetchData = (url, config, action) => async dispatch => {
+  try {
+    const { data } = await axios.get(url, config);
+    const result = data.data;
+    console.log(result);
+    dispatch(action(result));
+  } catch (err) {
+    return console.log(err);
+  }
+};
+
+export const pullData = (url, body, config, action) => async dispatch => {
+  try {
+    const { data } = await axios.post(url, body, config);
+    const result = data.data;
+    dispatch(action(result));
+  } catch (err) {
+    return console.log(err);
+  }
+};
+
 // actions creator functions
 // action creator: sign
-export const signIn = accessToken => {
+export const signIn = ({ accessToken }) => {
   return {
     type: SIGN_IN,
     payload: { accessToken },
@@ -26,7 +50,7 @@ export const signOut = () => {
 };
 
 // action creator: user
-export const getUserInfo = userData => {
+export const getUserInfo = ({ userData }) => {
   return {
     type: GET_USER_INFO,
     payload: userData,
@@ -34,7 +58,7 @@ export const getUserInfo = userData => {
 };
 
 //action creator: booking
-export const checkBook = bookingDatas => {
+export const checkBook = ({ bookingDatas }) => {
   console.log(bookingDatas);
   return {
     type: CHECK_BOOK,
@@ -43,7 +67,7 @@ export const checkBook = bookingDatas => {
 };
 
 // action creator: futsal
-export const getAllFutsalInfo = futsalDatas => {
+export const getAllFutsalInfo = ({ futsalDatas }) => {
   return {
     type: GET_ALLFUTSAL_INFO,
     payload: futsalDatas,
