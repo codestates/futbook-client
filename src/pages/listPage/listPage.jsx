d16c260c29b0c8ae223a2af056dd36facd125ff4
민제현 <alswpgus0@gmail.com>
2021-05-05 03:02:23 +0900

    feature_listPageConnectServer_#36:Modified listPage

--- src/pages/listPage/listPage.jsx
+++ src/pages/listPage/listPage.jsx
@@ -1,14 +1,44 @@
 import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import styles from "./listPage.module.css";
 import ListItem from "../../components/list_item/list_item";
 import Navbar from "../../components/navbar/navbar";
 import Footer from "../../components/footer/footer";
 import Modal from "../../components/modal/modal";
-function ListPage() {
+const ListPage = () => {
+  const futsalState = useSelector(state => state.futsalReducer);
+
   const [showModal, setShowModal] = useState(false);
 
+  const [futsalDatas, setFutsalDatas] = useState(futsalState.futsalData);
+
   const openModal = () => {
-    setShowModal((prev) => !prev);
+    setShowModal(prev => !prev);
+  };
+
+  const makePriceFormat = price => {
+    price = String(price).slice(0, 3) + "," + String(price).slice(3);
+    return `${price}원`;
+  };
+
+  const makeDateFormat = () => {
+    const aux = num => {
+      if (num < 10) {
+        return "0" + String(num);
+      } else {
+        return String(num);
+      }
+    };
+
+    const today = new Date();
+
+    const yyyy = today.getFullYear();
+    const month = aux(today.getMonth() + 1);
+    const dd = aux(today.getDate());
+
+    const format = [yyyy, month, dd].join("/");
+
+    return format;
   };
 
   useEffect(() => {
@@ -24,57 +54,21 @@ function ListPage() {
         <div className={styles.title}>
           <h1>풋살장 예약하기</h1>
         </div>
-        <ListItem
-          openModal={openModal}
-          img="/images/inside_football1.jpeg"
-          date="2021/05/01"
-          title="가 풋살장"
-          address="서울특별시 강남구 코드로 1길 스테이츠빌딩 1층"
-          label="실내"
-          price="11,0000원"
-        />
-
-        <ListItem
-          openModal={openModal}
-          img="/images/inside_football2.jpeg"
-          date="2021/05/01"
-          title="나 풋살장"
-          address="서울특별시 강남구 코드로 2길 스테이츠빌딩 2층"
-          label="실내"
-          price="12,0000원"
-        />
-
-        <ListItem
-          openModal={openModal}
-          img="/images/outside_football1.jpeg"
-          date="2021/05/01"
-          title="다 풋살장"
-          address="서울특별시 강남구 코드로 3길 스테이츠빌딩 3층"
-          label="실외"
-          price="13,0000원"
-        />
-        <ListItem
-          openModal={openModal}
-          img="/images/outside_football2.jpeg"
-          date="2021/05/01"
-          title="라 풋살장"
-          address="서울특별시 강남구 코드로 4길"
-          label="실외"
-          price="14,0000원"
-        />
-        <ListItem
-          openModal={openModal}
-          img="/images/outside_football3.jpeg"
-          date="2021/05/01"
-          title="마 풋살장"
-          address="서울특별시 강남구 코드로 5길"
-          label="실외"
-          price="15,0000원"
-        />
+        {futsalDatas.map((data, idx) => (
+          <ListItem
+            openModal={openModal}
+            img={data.thumnail}
+            date={makeDateFormat()}
+            title={data.name}
+            address={data.location}
+            label={data.stageType}
+            price={makePriceFormat(data.fee)}
+          />
+        ))}
       </div>
       <Footer />
     </>
   );
-}
+};
 
 export default ListPage;
