2b70a29bbf48c4b84c5d1bb983392df5d8c5de69
민제현 <alswpgus0@gmail.com>
2021-05-06 06:27:00 +0900

    feature_checkPageConnect_#52:Modified checkPage to connect server

--- src/components/check_item/check_item.jsx
+++ src/components/check_item/check_item.jsx
@@ -1,32 +1,44 @@
 import React from "react";
 import styles from "./check_item.module.css";
 
-const CheckItem = ({ img, date, title, address, label, price }) => {
+const CheckItem = ({
+  futsalData,
+  bookDate,
+  makePriceFormat,
+  handleBtnDelete,
+}) => {
   return (
     <div className={styles.item}>
-      <img className={styles.image} src={img} alt="check" />
+      <img className={styles.image} src={futsalData.thumnail} alt="check" />
       <div className={styles.info}>
         <div className={styles.info_top}>
-          <p className={styles.date}>{date}</p>
-          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.date}>{bookDate}</p>
+          <h3 className={styles.title}>{futsalData.name}</h3>
           <p>&nbsp;</p>
-          <p className={styles.address}>{address}</p>
-          <p className={styles.price}>{`${price}원`}</p>
+          <p className={styles.address}>{futsalData.location}</p>
+          <p className={styles.price}>{makePriceFormat(futsalData.fee)}</p>
         </div>
         <div className={styles.info_bottom}>
           <div>
             <p
               className={
-                label === "실외"
+                `${futsalData.stageType}` === "실외"
                   ? `${styles.label}`
                   : `${styles.label} ${styles.inside}`
               }
             >
-              {label}
+              {futsalData.stageType}
             </p>
           </div>
           <div className={styles.cancel}>
-            <button className={styles.cancel_btn}>예약취소</button>
+            <button
+              className={styles.cancel_btn}
+              onClick={() => {
+                handleBtnDelete(futsalData.id, bookDate);
+              }}
+            >
+              예약취소
+            </button>
           </div>
         </div>
       </div>
