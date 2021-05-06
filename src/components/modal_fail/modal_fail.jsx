f9cc1294f92dd70935e389cd16b35b76bbc1132d
민제현 <alswpgus0@gmail.com>
2021-05-06 13:08:34 +0900

    feature_ModifySFModalCSS_#48:Modified ModalFail component format and CSS

--- src/components/modal_fail/modal_fail.jsx
+++ src/components/modal_fail/modal_fail.jsx
@@ -13,14 +13,14 @@ const ModalFail = ({ showFail, setShowFail }) => {
     transform: showFail ? `translateY(5%)` : `translateY(-100%)`,
   });
 
-  const closeFail = (e) => {
+  const closeFail = e => {
     if (backRef.current === e.target) {
       setShowFail(false);
     }
   };
 
   const keyPress = useCallback(
-    (e) => {
+    e => {
       if (showFail && e.key === "Escape") {
         setShowFail(false);
       }
@@ -38,28 +38,28 @@ const ModalFail = ({ showFail, setShowFail }) => {
         <div className={styles.back} onClick={closeFail} ref={backRef}>
           <animated.div style={animation}>
             <div className={styles.wrap}>
-              <div className={styles.left}></div>
-              <div className={styles.right}>
-                <div className={styles.title}>
-                  예약실패 &nbsp;
-                  <i className={`fas fa-times ${styles.fail}`}></i>
-                </div>
-                <p className={styles.msg}>
-                  *해당 날짜는 이미 예약이 완료되었습니다.
-                </p>
-                <button
-                  className={styles.btn_fail}
-                  onClick={() => setShowFail((prev) => !prev)}
-                >
-                  닫기
-                </button>
-              </div>
               <div
                 className={styles.close}
-                onClick={() => setShowFail((prev) => !prev)}
+                onClick={() => setShowFail(prev => !prev)}
               >
                 <i className="fas fa-times"></i>
               </div>
+
+              <div className={styles.container}>
+                <div className={styles.title}>
+                  <p className={styles.title_msg}>예약실패</p>
+                  <i className="fas fa-times"></i>
+                </div>
+                <p className={styles.side_msg}>
+                  *해당 날짜는 이미 예약이 완료되었습니다.
+                </p>
+              </div>
+              <button
+                className={styles.btn_close}
+                onClick={() => setShowFail(prev => !prev)}
+              >
+                닫기
+              </button>
             </div>
           </animated.div>
         </div>
